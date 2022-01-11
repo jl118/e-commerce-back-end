19be46c2c3770b668b35e974d2b8720002cb0376
jl118 <jen.liebelt@gmail.com>
2022-01-11 18:19:41 -0500

    fixed issue with product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,7 +10,8 @@ router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag, through: ProductTag }]
-    })
+    });
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
