1c3d6af47eb752d37f36fafad15185046e88517b
jl118 <jen.liebelt@gmail.com>
2022-01-11 17:13:59 -0500

    added find tag by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,23 +20,39 @@ router.get('/', async (req, res) => {
 });
 
 // find a single tag by its `id`
-router.get('/:id', (req, res) => {
-  
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  // including its associated Product data
+  try{
+    const tagData = await Tag.findByPk(req.params.id, { 
+      include: [{
+        model: Product, 
+        through: ProductTag
+      }]
+    });
+    if(!tagData) {
+      res.status(404).json({ 
+        message: "No tag found by that ID."
+      });
+      return;
+    };
+    res.status(200).json(tagData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 // create a new tag
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   
 });
 
 // update a tag's name by its `id` value
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   
 });
 
 // delete on tag by its `id` value
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   
 });
 
