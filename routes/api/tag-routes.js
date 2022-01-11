d9e43ce4d33d2d8df60c71893446f7e55446c2e8
jl118 <jen.liebelt@gmail.com>
2022-01-11 17:19:13 -0500

    worked on tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
 // find a single tag by its `id`
 router.get('/:id', async (req, res) => {
   // including its associated Product data
-  try{
+  try {
     const tagData = await Tag.findByPk(req.params.id, { 
       include: [{
         model: Product, 
@@ -43,17 +43,52 @@ router.get('/:id', async (req, res) => {
 
 // create a new tag
 router.post('/', async (req, res) => {
-  
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // update a tag's name by its `id` value
 router.put('/:id', async (req, res) => {
-  
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id:req.params.id
+      }
+    });
+    if(!tagData) {
+      res.status(404).json({ 
+        message: "No tag found by that ID."
+      })
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // delete on tag by its `id` value
 router.delete('/:id', async (req, res) => {
-  
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!tagData) {
+      res.status(404).json({ 
+        message: "No tag found by that ID." 
+      });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
